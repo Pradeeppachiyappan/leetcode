e6c3120b9dff0d94b861686bc00f9b062b8d5ccc
P.PRADEEPRAJ <118707347+Pradeeppachiyappan@users.noreply.github.com>
2025-04-23 21:19:13 +0530

    Time: 1 ms (67.3%), Space: 41.4 MB (32.89%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        List<List<Integer>> pascal=new ArrayList<>();
+        List<Integer> pascal=new ArrayList<>();
         int i;
         for(i=0;i<=rowIndex;i++){
             List<Integer> row = new ArrayList<>();
@@ -11,13 +11,12 @@ class Solution {
 
                 }
                 else{
-                    List<Integer> PreviousRow = pascal.get(i-1);
-                    row.add(PreviousRow.get(j)+PreviousRow.get(j-1));
+                    int sum = pascal.get(j-1) + pascal.get(j);
+                    row.add(sum);
                 }
             }
-            pascal.add(row);
+            pascal=row;
         }
-        List<Integer> list=pascal.get(pascal.size()-1);
-        return list;
+        return pascal;
     }
 }
